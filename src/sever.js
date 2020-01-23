30d595e7a9412eeb0a4925c8679f5bd00c221f67
Natanael <taelima1997@gmail.com>
2020-01-23 00:51:44 -0300

    added views ,styles and routes basics

--- src/sever.js
+++ src/sever.js
@@ -1,7 +1,17 @@
 import express from 'express'
 
 const server = express()
+
+server.set('view engine', 'ejs')
+server.use(express.static('public'))
 // init server
+server.get('/', (req, res) => {
+  res.render('home')
+})
+server.get('/vaga', (req, res) => {
+  res.render('vaga')
+})
+
 server.listen(3333, () => {
   console.log('port 3333')
 })
