566022835f306831c9c453ad263d34301dd5bd72
Natanael <taelima1997@gmail.com>
2020-01-23 19:58:20 -0300

    added styles and animations to screens vaga and added vaga

--- src/sever.js
+++ src/sever.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import sqlite from 'sqlite'
+import bodyParser from 'body-parser'
 
 const server = express()
 const dbConnection = sqlite.open('jobify.sqlite', { Promise })
 
 server.set('view engine', 'ejs')
 server.use(express.static('public'))
+server.use(bodyParser.urlencoded({ extended: true }))
 // init server
 server.get('/', async (req, res) => {
   const db = await dbConnection
@@ -26,6 +28,28 @@ server.get('/vaga/:id', async (req, res) => {
 
 server.get('/admin', (req, res) => {
   res.render('admin/home')
+})
+
+server.get('/admin/vaga', async (req, res) => {
+  const db = await dbConnection
+  const vagas = await db.all('select * from vaga')
+  res.render('admin/vaga', { vagas })
+})
+// create new vaga
+server.post('/admin/create-vaga', async (req, res) => {
+  const { title, description } = req.body
+  const db = await dbConnection
+  await db.all(
+    `insert into vaga (title,description) values ('${title}','${description}')`
+  )
+  res.redirect('vaga')
+})
+// remove opportunity
+server.get('/admin/remove-opportunity/:id', async (req, res) => {
+  const { id } = req.params
+  const db = await dbConnection
+  await db.all(`delete from vaga where id = ${id}`)
+  res.redirect('vaga')
 })
 
 const init = async () => {
